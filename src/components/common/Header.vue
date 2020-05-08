f93a8b662be8b56c55458829e88c5ea26caba2c1
FCD_廖宇 <yu.liao@fenrir-inc.com.cn>
2020-05-08 17:21:46 +0800

    是

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -5,7 +5,7 @@
             <i v-if="!collapse" class="el-icon-s-fold"></i>
             <i v-else class="el-icon-s-unfold"></i>
         </div>
-        <div class="logo">房屋租赁后台管理</div>
+        <div class="logo">RENTALHOUSE管理平台</div>
         <div class="header-right">
             <div class="header-user-con">
                 <!-- 消息中心 -->
